7ce2c7b8e3a342f1c5f421860835039b55ea841a
Daryl Chang <dlchang@google.com>
2013-07-12 11:46:15 -0700

    modified indents

--- main.py
+++ main.py
@@ -22,7 +22,10 @@ YOUTUBE_API_VERSION = "v3"
 class MainHandler(webapp2.RequestHandler):
    
    def get(self):
-        youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+        youtube = build(
+          YOUTUBE_API_SERVICE_NAME, 
+          YOUTUBE_API_VERSION, 
+          developerKey=DEVELOPER_KEY)
         search_response = youtube.search().list(
           q="Hello",
           part="id,snippet",
@@ -35,11 +38,14 @@ class MainHandler(webapp2.RequestHandler):
         
         for search_result in search_response.get("items", []):
           if search_result["id"]["kind"] == "youtube#video":
-              videos.append("%s (%s)" % (search_result["snippet"]["title"], search_result["id"]["videoId"]))
+              videos.append("%s (%s)" % (search_result["snippet"]["title"], 
+                search_result["id"]["videoId"]))
           elif search_result["id"]["kind"] == "youtube#channel":
-              channels.append("%s (%s)" % (search_result["snippet"]["title"], search_result["id"]["channelId"]))
+              channels.append("%s (%s)" % (search_result["snippet"]["title"], 
+                search_result["id"]["channelId"]))
           elif search_result["id"]["kind"] == "youtube#playlist":
-              playlists.append("%s (%s)" % (search_result["snippet"]["title"], search_result["id"]["playlistId"]))
+              playlists.append("%s (%s)" % (search_result["snippet"]["title"], 
+                search_result["id"]["playlistId"]))
         
         template_values = {
          'videos': videos,
